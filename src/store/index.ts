6db794c3d93a80db7383959c015a433d15b9dabd
Kévin Gossé <gossekevin1@gmail.com>
2016-05-03 18:22:39 +0200

    Add todostore

---
+++ src/store/index.ts
@@ -0,0 +1,61 @@
+export class Todo {
+    done: Boolean;
+
+    private _text: String;
+    get text() {
+        return this._text;
+    }
+    set text(value: String) {
+        this._text = value.trim();
+    }
+
+    constructor(text: String) {
+        this.done = false;
+        this.text = text.trim();
+    }
+}
+
+export class TodoStore {
+    todos: Array<Todo>;
+
+    constructor() {
+        let persistedTodos = JSON.parse(localStorage.getItem('todos') || '[]');
+        // Normalize back into classes
+        this.todos = persistedTodos.map( (todo: {_text: String, done: Boolean}) => {
+            let ret = new Todo(todo._text);
+            ret.done = todo.done;
+            return ret;
+        });
+    }
+
+    private updateStore() {
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
+
+    private getTasks(done: Boolean) {
+        return this.todos.filter((todo: Todo) => todo.done === done);
+    }
+
+    removeDoneTasks() {
+        this.todos = this.getTasks(false);
+        this.updateStore();
+    }
+
+    getRemaining() {
+        return this.getTasks(false);
+    }
+
+    getDoneTasks() {
+        return this.getTasks(true);
+    }
+
+    toggleTask(todo: Todo) {
+        todo.done = !todo.done;
+        this.updateStore();
+    }
+
+    add(text: String) {
+        this.todos.push(new Todo(text));
+        this.updateStore();
+    }
+}
